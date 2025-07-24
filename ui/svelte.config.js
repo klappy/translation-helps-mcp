a785bb72949ab4559ff6ae00f6bd8281b3c5bd95
Chris Klapp <klappy@users.noreply.github.com>
2025-07-24 15:26:20 -0400

    Simplify Cloudflare adapter config: remove custom routes to fix worker bundling

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -3,13 +3,7 @@ import adapter from '@sveltejs/adapter-cloudflare';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// Cloudflare Pages configuration with Functions support
-			routes: {
-				include: ['/*'],
-				exclude: ['/build/*', '/_app/*']
-			}
-		})
+		adapter: adapter()
 	}
 };
 
