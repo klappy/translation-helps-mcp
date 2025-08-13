35a3dbc1c979f0a4804b6c493095cb8b73ea6189
Automation <devnull@example.com>
2025-08-13 17:48:06 -0400

    6.2.1

--- src/version.ts
+++ src/version.ts
@@ -3,7 +3,7 @@
  * SINGLE SOURCE OF TRUTH for server version. Do not edit manually.
  */
 
-export const VERSION = "6.2.0";
+export const VERSION = "6.2.1";
 
 export function getVersion(): string {
   return VERSION;
